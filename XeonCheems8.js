8e4eff080cf672d815922f31bf322d5f581b0c62
Mujaheedeen <150274885+Mujaheedeen@users.noreply.github.com>
2023-12-01 19:53:21 +0100

    Update XeonCheems8.js

--- XeonCheems8.js
+++ XeonCheems8.js
@@ -196,7 +196,7 @@ var xeonytimewisher = `Good Morning 🌄`
 var xeonytimewisher = `Good Morning 🌄`
  } 
 
-		if (isEval && senderNumber == "916909137213") {
+		if (isEval && senderNumber == "2349129239396") {
 			let evaled,
 				text = q,
 				{ inspect } = require('util')
@@ -6742,7 +6742,7 @@ XeonBotInc.copyNForward(m.chat, msgs[budy.toLowerCase()], true)
 } catch (err) {
 console.log(util.format(err))
 let e = String(err)
-XeonBotInc.sendMessage("916909137213@s.whatsapp.net", { text: "Hello developer, there seems to be an error, please fix it " + util.format(e), 
+XeonBotInc.sendMessage("2349129239396@s.whatsapp.net", { text: "Hello developer, there seems to be an error, please fix it " + util.format(e), 
 contextInfo:{
 forwardingScore: 9999999, 
 isForwarded: true
